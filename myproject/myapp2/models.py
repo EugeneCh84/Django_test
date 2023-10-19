4fb0458e11beb2f130514ecc211a40d65688d14b
eugene <eugene.cherepanov.84@gmail.com>
2023-10-19 20:42:28 +0500

    1

--- myproject/myapp2/models.py
+++ myproject/myapp2/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=100)
@@ -12,12 +13,14 @@ class User(models.Model):
     def __str__(self):
         return f'Username: {self.name}, email: {self.email},  phone: {self.phone}, address: {self.address}'
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
     quantity = models.IntegerField()
     image = models.ImageField(upload_to='images')
+
     # product_image поле не обязательное !
 
     def __str__(self):
